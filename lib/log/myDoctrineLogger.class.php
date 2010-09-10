ab9cd44737789c3e971ff59396445a4e7aa815ee
Svel <Svel.Sontz@gmail.com>
2010-09-10 07:01:59 +0400

    t1718: переписал логгер

--- lib/log/myDoctrineLogger.class.php
+++ lib/log/myDoctrineLogger.class.php
@@ -6,7 +6,7 @@
  * @author     Svel.Sontz <svel.sontz@gmail.com>
  * @version    0.1
  */
-class myDoctrineLogger
+class myDoctrineLogger extends sfLogger
 {
 
     protected
@@ -15,128 +15,73 @@ class myDoctrineLogger
 
 
     /**
-     * Конструктор
-     */
-    public function __construct(sfEventDispatcher $dispatcher, $options = array())
-    {
-        $this->initialize($dispatcher, $options);
-
-        if (!isset($options['auto_shutdown']) || $options['auto_shutdown']) {
-            register_shutdown_function(array($this, 'shutdown'));
-        }
-    }
-
-
-    /**
      * Инициализация логгера
      */
     public function initialize(sfEventDispatcher $dispatcher, array $options = array())
     {
         $this->dispatcher = $dispatcher;
-        $this->setOptions($options);
-
-        $this->dispatcher->connect('app.activity', array($this, 'listenToLogEvent'));
+        $this->options = $options;
     }
 
 
     /**
-     *
+     * Заглушка
      */
-    public function log($subject, array $parameters = array())
-    {
-        $this->doLog($subject, $parameters);
-    }
-
-
-    /**
-     *
-     */
-    protected function doLog($subject, array $parameters = array())
-    {
-        $log = new myDoctrineLoggerEvent();
-
-        $log->setState($parameters['state']);
-        $log->setComponent($parameters['component']);
-        $log->setLabel($parameters['name']);
-        $log->setResult($parameters['description']);
-        $log->setContext('some');
-
-        // TODO убрать хардкод на метод получения идентификатора пользователя
-        if (sfContext::hasInstance()) {
-            $context = sfContext::getInstance();
-            $userId = $context->getUser()->getUserRecord()->getId();
-            $log->setUserId($userId);
-        }
-
-
-        $log->save();
-    }
-
-
-    /**
-     * Установить опции
-     */
-    protected function setOptions(array $options = array(), $reset = false)
-    {
-        if ($reset) {
-            $this->options = array();
-        }
-
-        foreach ($options as $name => $value) {
-            $this->setOption($name, $value);
-        }
-
-        return $this;
-    }
-
-
-    /**
-     * Установить параметр
-     */
-    protected function setOption($name, $value)
-    {
-        $this->options[(string) $name] = $value;
-
-        return $this;
-    }
-
-
-    /**
-     * Получить параметр
-     */
-    protected function getOption($name)
-    {
-        if (array_key_exists((string) $name, $this->options)) {
-            return $this->options[(string) $name];
-        }
-
-        return false;
-    }
-
-
-    /**
-     * Проверить, существует ли параметр
-     */
-    protected function hasOption($name)
-    {
-        return array_key_exists((string) $name, $this->options);
-    }
-
-
-    /**
-     * Действия при выключении
-     */
-    public function shutdown()
-    {
-    }
+    protected function doLog($message, $priority)
+    {}
 
 
     /**
      * Обрабатывает событие из dispatcher
      */
-    public function listenToLogEvent(myLogEvent $event)
+    public function listenToLogEvent(sfEvent $event)
     {
-        $this->log($event->getSubject(), $event->getParameters());
+        $subject = $event->getSubject();
+        $parameters = $event->getParameters();
+
+        try {
+            $record = new myDoctrineLoggerEvent();
+        } catch (Exception $e) {
+            new sfDatabaseManager(ProjectConfiguration::getActive());
+            $record = new myDoctrineLoggerEvent();
+        }
+
+        // Статус: fail, info, notice, warning
+        if (isset($parameters['state'])) {
+            $record->setState($parameters['state']);
+        }
+
+        // Идентификатор связанного объекта, если есть
+        if (isset($parameters['object'])) {
+            $record->setModelId((int) $parameters['object']);
+        }
+
+        // Идентификатор пользователя - инициатора события, если есть
+        if (isset($parameters['user'])) {
+            if (is_object($parameters['user'])) {
+                $parameters['user'] = $parameters['user']->getId();
+            }
+            $record->setUserId((int) $parameters['user']);
+        }
+
+        // Компонент приложения
+        $record->setComponent(isset($parameters['component']) ? (string) $parameters['component'] : 'UNKNOWN');
+        // Название события
+        if (!isset($parameters['name'])) {
+            throw new sfException('Необходимо назвать событие: заполнить параметр ["name"]');
+        }
+        if (!isset($parameters['description'])) {
+            throw new sfException('Необходимо назвать событие: заполнить параметр ["description"]');
+        }
+        $record->setLabel((string) $parameters['name']);
+        // расшифровка результата
+        $record->setResult((string) $parameters['description']);
+        // Данные события
+        if(isset($parameters['env'])) {
+            $record->setContext((string) $parameters['env']);
+        }
+
+        $record->save();
     }
 
 }
